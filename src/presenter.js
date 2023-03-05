df65a54aa003d64c237ca409114384d6afac7f92
Jacco126 <jonatan.terceros@ucb.edu.bo>
2023-03-05 18:58:40 -0400

    Ingresar descripcion y mostrarlo

--- src/presenter.js
+++ src/presenter.js
@@ -2,16 +2,16 @@ import sumar from "./sumador";
 import multiplicar from "./multiplicador";
 
 const titulo = document.querySelector("#titulo");
-//const descripcion = document.querySelector("#descripcion");
+const descripcion = document.querySelector("#descripcion");
 const div = document.querySelector("#message-div");
-//const div2 = document.querySelector("#message2-div");
+const div2 = document.querySelector("#message2-div");
 const form = document.querySelector("#message-form");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   div.innerHTML = "titulo: " +  titulo.value;
-  //div2.innerHTML = "descripcion: " + descripcion.value;
+  div2.innerHTML = "descripcion: " + descripcion.value;
 
 
 });
