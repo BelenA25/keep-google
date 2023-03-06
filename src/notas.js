c4de1120c0b4045592f0e500b02d697bc7d10b03
Jacco126 <jonatan.terceros@ucb.edu.bo>
2023-03-05 21:36:01 -0400

    mejora de codigo del presenter moviendo a la clase ListaNotas

--- src/notas.js
+++ src/notas.js
@@ -1,3 +1,4 @@
+import { Nota } from './nota.js';
 export class ListaNotas{
     constructor(){
         this.notas = []
@@ -10,5 +11,41 @@ export class ListaNotas{
     }
     getTamanio(){
         return this.notas.length;
+    }
+    mostrarNotas(){
+      
+        const newDiv = document.createElement("div");
+        const newDiv2 = document.createElement("div");
+      
+        const currentDiv = document.getElementById("message-div");
+        const currentDiv2 = document.getElementById("message2-div");
+
+
+      
+        for (let i = 0; i < this.getTamanio(); i++) 
+        {
+          const newContentTitulo = document.createTextNode("titulo: " + this.notas[i].getTitulo());
+          const newContentDescripcion = document.createTextNode("descripcion: " + this.notas[i].getDescripcion());
+          
+          newDiv.appendChild(newContentTitulo);
+          newDiv2.appendChild(newContentDescripcion);
+      
+          const tituloDiv = document.createElement("div");
+          const descripcionDiv = document.createElement("div");
+      
+          tituloDiv.appendChild(newContentTitulo);
+          descripcionDiv.appendChild(newContentDescripcion);
+      
+          newDiv.appendChild(tituloDiv);
+          newDiv2.appendChild(descripcionDiv);
+
+
+
+        }
+        currentDiv.innerHTML = '';
+        currentDiv2.innerHTML = '';
+        
+        currentDiv.appendChild(newDiv);
+        currentDiv2.appendChild(newDiv2);
     }
 }
--- src/presenter.js
+++ src/presenter.js
@@ -3,38 +3,18 @@ import { ListaNotas } from './notas.js';
 
 const titulo = document.querySelector("#titulo");
 const descripcion = document.querySelector("#descripcion");
-const div = document.querySelector("#message-div");
-const div2 = document.querySelector("#message2-div");
 const form = document.querySelector("#message-form");
 const notas = new ListaNotas();
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
+  
   const nota = new Nota(titulo.value, descripcion.value);
   notas.aniadirNota(nota);
 
-  for (let i = 0; i < notas.getTamanio(); i++) 
-  {
-    const newDiv = document.createElement("div");
-    const newDiv2 = document.createElement("div2");
+  notas.mostrarNotas();
 
-    const newContentTitulo = document.createTextNode("titulo: " + notas.getListaNotas()[i].getTitulo());
-    const newContentDescripcion = document.createTextNode("descripcion: " + notas.getListaNotas()[i].getDescripcion());
-    
-    newDiv.appendChild(newContentTitulo);
-    newDiv2.appendChild(newContentDescripcion);
-
-    const currentDiv = document.getElementById("message-div");
-    const currentDiv2 = document.getElementById("message2-div");
-
-    currentDiv.insertBefore(newDiv, currentDiv);
-    currentDiv2.insertBefore(newDiv2, currentDiv2);
-    // document.getElementById("#message-div").appendChild(div);
-    // div.innerHTML = "titulo: " + notas.getListaNotas()[i].getTitulo();
-    // div2.innerHTML = "descripcion: " + notas.getListaNotas()[i].getDescripcion();
-    // document.body.innerHTML += ''
-  }
   
 });
 
