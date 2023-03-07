25bb1c1cced27c3169862e596c2bc8db1589bfb5
Belencita <beckiar23@gmail.com>
2023-03-07 09:28:51 -0400

    Mejora del resultado de busqueda de notas

--- src/presenter.js
+++ src/presenter.js
@@ -4,7 +4,7 @@ import { ListaNotas } from './notas.js';
 const titulo = document.querySelector("#titulo");
 const descripcion = document.querySelector("#descripcion");
 const form = document.querySelector("#message-form");
-const form2 = document.querySelector("#buscador-form");
+const formBuscador = document.querySelector("#buscador-form");
 const form3 = document.querySelector("#eliminar-form");
 const notas = new ListaNotas();
 const busqueda = document.querySelector("#buscador");
@@ -19,15 +19,14 @@ form.addEventListener("submit", (event) => {
 
 });
 
-form2.addEventListener("submit", (event) => {
+formBuscador.addEventListener("submit", (event) => {
   event.preventDefault();
-
 
   const notaEncontrada = notas.buscarPorTitulo(busqueda.value);
 
   if (notaEncontrada) {
-    const tituloDiv = document.getElementById("message-div");
-    const descripcionDiv = document.getElementById("message2-div");
+    const tituloDiv = document.getElementById("resultadoTitulo");
+    const descripcionDiv = document.getElementById("resultadoDescripcion");
     
     tituloDiv.innerHTML = 'Título encontrado:' + notaEncontrada.getTitulo();
     descripcionDiv.innerHTML = 'Descripción Encontrada: ' + notaEncontrada.getDescripcion();
